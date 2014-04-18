4eb719d9366d8dfee87547139e1cc68ff38cf78f
furajium <fy.sasaki@gmail.com>
2014-04-18 10:13:05 +0000

    change init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd $(dirname $0)
 for dotfile in .?*; do
-  if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+  if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitignore' ]
   then
     ln -Fis "$PWD/$dotfile" $HOME
   fi
